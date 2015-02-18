ae1b94519018616bb001c4bf1131f7efab1128d1
Daniel Alves <daniel@danielalves.me>
2015-02-18 21:15:16 +0000

    Fix Vim tabsize mess

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout' }
   root to: 'home#index'
-	namespace :dashboard do
+  namespace :dashboard do
     root to: 'projects#index'
     resource :site, only: [:edit, :update]
     resources :projects, :pages, :site_assets
-	end
+  end
 
-	scope constraints: { username: /[a-z]+/ } do
-		get '/:username', to: 'site/pages#show', defaults: { page_slug: 'home' }, as: :site
-		get '/:username/projects/:project_slug', to: 'site/pages#show', defaults: { page_slug: 'project' }, as: :site_project
-		get '/:username/:page_slug', to: 'site/pages#show', as: :site_page
-		get '/:username/assets/:file_name', to: 'site/site_assets#show', as: :site_asset, constraints: { file_name: /[a-z_]+\.(js|css)/ }
-	end
+  scope constraints: { username: /[a-z]+/ } do
+    get '/:username', to: 'site/pages#show', defaults: { page_slug: 'home' }, as: :site
+    get '/:username/projects/:project_slug', to: 'site/pages#show', defaults: { page_slug: 'project' }, as: :site_project
+    get '/:username/:page_slug', to: 'site/pages#show', as: :site_page
+    get '/:username/assets/:file_name', to: 'site/site_assets#show', as: :site_asset, constraints: { file_name: /[a-z_]+\.(js|css)/ }
+  end
 end
