07082e25fd92f1053c66171e634b6916d8e2667a
Daniel Alves <daniel@danielalves.me>
2015-02-22 19:46:01 +0000

    Fix User factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
   factory :user do
     sequence(:email) { |n| "user-#{n}@example.com" }
     sequence(:name) { |n| "User #{n}" }
+    sequence(:username) { |n| "user#{n}" }
     password '12345678'
   end
 end
